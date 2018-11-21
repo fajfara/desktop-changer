e03eba79e72849f5b2f2da8152299f55cfbf9ee2
fajfara <a.fajfar95@gmail.com>
2018-11-21 20:29:07 +0100

    Adding time of day detection and delete images if folder too large

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ background_path = '/home/fajfara/Pictures/daily-backgrounds/'
 datumDanes = '{date:%Y-%m-%d}'.format( date=datetime.datetime.now())
 
 # Weather api variables
-apiKeyWeather = 'WEATHER-API-KEY'
+apiKeyWeather = 'API'
 userCity = 'Kranj'
 urlW = 'https://api.openweathermap.org/data/2.5/weather?q=' + userCity + '&APPID=' + apiKeyWeather
 
 # unsplash api key
-clientId = 'UNSPLASH-API-KEY'
+clientId = 'UNSPLASH'
 
 def changeBackground(data):
     randomPicIndex = randint(0, len(data['results']) - 1)
@@ -30,6 +30,29 @@ def changeBackground(data):
     os.system("gsettings set org.gnome.desktop.background picture-uri file:/home/fajfara/Pictures/daily-backgrounds/"+ filename + ".jpeg")
     print("Check:" + background_path + filename)
 
+def checkIfNightOrDay():
+    now = datetime.datetime.now()
+    if now.hour > 7 and now.hour < 18:
+        timeOfDay = 'day'
+        return timeOfDay
+    else:
+        timeOfDay = 'night'
+        return timeOfDay
+
+def checkFolder():
+    path, dirs, files = next(os.walk(background_path))
+    file_count = len(files)
+    if file_count > 10:
+        for the_file in os.listdir(background_path):
+            file_path = os.path.join(background_path, the_file)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+                # elif os.path.isdir(file_path): shutil.rmtree(file_path)
+            except Exception as e:
+                print(e)
+    else:
+        return
 
 
 
@@ -39,15 +62,20 @@ rW = requests.get(urlW)
 weatherData = rW.json()
 
 # Unsplash settings
-photoQuery =  weatherData['weather'][0]['main']
+
+photoQuery = weatherData['weather'][0]['main']  + ' ' + checkIfNightOrDay()
 url = 'https://api.unsplash.com/search/photos'
 parametersUnsplash = {
     'client_id': clientId,
     'query': photoQuery,
-    'per_page': 30
+    'per_page': 100
 }
+
+print(parametersUnsplash['query'
+                         ''])
 r = requests.get(url, params=parametersUnsplash)
 
+checkFolder()
 changeBackground((r.json()))
 
 
